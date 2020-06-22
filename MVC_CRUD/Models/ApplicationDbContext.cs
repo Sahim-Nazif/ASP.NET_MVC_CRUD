d305ab2bf1ffbd47bfdb87cab3febfd6dd9933fa
Sahim Nazif <nsahim@my.centennialcollege.ca>
2020-06-22 09:27:52 -0400

    created applicationDbcontext

---
+++ MVC_CRUD/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options ):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
