af5809db04802bb562050e758d1f91b5ee7a2fc8
Sahim Nazif <nsahim@my.centennialcollege.ca>
2020-06-21 21:52:47 -0400

    Created model & added fields

---
+++ MVC_CRUD/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
