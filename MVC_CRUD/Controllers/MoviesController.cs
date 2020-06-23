ff1261df22fc338886fdd2c7496507a5068ebd54
Sahim Nazif <nsahim@my.centennialcollege.ca>
2020-06-23 17:42:48 -0400

    updated movieController

--- MVC_CRUD/Controllers/MoviesController.cs
+++ MVC_CRUD/Controllers/MoviesController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_CRUD.Models;
 
 namespace MVC_CRUD.Controllers
 {
     public class MoviesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public MoviesController( ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult>GetAll()
+        {
+            return Json(new { data = await _db.Movies.ToListAsync() });
+        }
+        #endregion
     }
 }
